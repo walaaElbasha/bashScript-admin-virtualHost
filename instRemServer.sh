d9165526ceb1485b5bb659c93eb7257fe8c1786c
walaaElbasha <52048396+walaaElbasha@users.noreply.github.com>
2021-03-12 16:38:50 +0200

    Update instRemServer.sh

--- instRemServer.sh
+++ instRemServer.sh
@@ -23,15 +23,18 @@ if [ $(dpkg --get-selections | grep apache2 | wc -l ) -ne  0 ]; then
 	read CH
 	case ${CH} in
 	"y")
-	echo "Removing apache2!"
-	##stopping the server
-	sudo service apache2 stop
-	sudo apt-get purge apache2 apache2-utils apache2.2-bin apache2-common
-	sudo apt-get autoremove
-	whereis apache2
-	apache2: /etc/apache2
-	sudo rm -rf /etc/apache2
-	;;
+		echo "Removing apache2!"
+		##stopping the server
+		sudo service apache2 stop
+		sudo apt-get purge apache2 apache2-utils apache2.2-bin apache2-common
+		sudo apt-get autoremove
+		whereis apache2
+		apache2: /etc/apache2
+		sudo rm -rf /etc/apache2
+		;;
+	"n")
+		echo "ok! Deletion is cancelled"
+		;;
 	esac
 else
 	echo "Apache2 doesnt exist!!!"
